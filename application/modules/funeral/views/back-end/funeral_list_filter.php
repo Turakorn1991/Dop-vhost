c17484c1971adfbb8f9f51dfd218426a0fb7b773
Turakorn <Turakorn@smartalliance.co.th>
2024-07-29 20:23:29 +0700

    commit funeral/funeral_list

--- application/modules/funeral/views/back-end/funeral_list_filter.php
+++ application/modules/funeral/views/back-end/funeral_list_filter.php
@@ -1,145 +1,177 @@
-
 <!-- search-->
 <div class="collapse" id="collapseExample">
   <div class="well" style="background-color: rgb(255, 255, 255);">
     <div class="container-fluid">
 
-       <div class="form-group row">
-         <div class="col-xs-12 col-sm-12"><h3><label>ค้นหา</label></h3></div>
-       </div>
+      <div class="form-group row">
+        <div class="col-xs-12 col-sm-12">
+          <h3><label>ค้นหา</label></h3>
+        </div>
+      </div>
 
-       <div class="form-group row ">
-           <div class="col-xs-12 col-sm-3">
-               <h3><label for="col1_filter">เลขประจำตัวประชาชน (ผู้สูงอายุ):</label></h3>
-           </div>
-           <div class="col-xs-12 col-sm-3">
-               <input data-column="1" type="text" class="form-control numberonly column_filter" id="col1_filter" placeholder="เลขประจำตัวประชาชน (13 หลัก)" maxlength="13">
-           </div>
-            <div class="col-xs-12 col-sm-6">
-             <div class="form-check">
-              <h3>
-               <label class="form-check-label">
-                 <input type="checkbox" class="form-check-input "  id="disablepid" >
-                 ไม่ระบุเลขบัตรประจำตัวประชาชน
-               </label>
-             </h3>
-             </div>
-
-           </div>
-       </div>
-
-       <div class="form-group row">
-         <div class="col-xs-12 col-sm-3">
-           <h3><label for="col2_filter">ชื่อตัว-ชื่อสกุล:</label></h3>
-         </div>
-         <div class="col-xs-12 col-sm-6">
-           <input data-column="2" type="text" class="form-control column_filter" id="col2_filter" placeholder="ชื่อตัว-ชื่อสกุล">
-         </div>
-       </div>
-
-       <div class="form-group row">
-         <div class="col-xs-12 col-sm-3" style="padding-top: 12px;" >
-           <h3><label for="col3_filter">เพศ:</label></h3>
-         </div>
-         <div class="col-xs-12 col-sm-3" style="padding-top: 12px;">
-
-                <select  class="form-control" id="gender">
-                <option value="">เพศ</option>
-                  <option value="1">ชาย (Male)</option>
-                  <option value="2">หญิง (Female)</option>
-                  <!-- <option value="0">0</option> -->
-                  <option value="ไม่ทราบ">ไม่ทราบ</option>
-                 
-               </select>
-         </div>
-         
-         <div class="col-xs-12 col-sm-1" style="padding-top: 12px;">
-                <h3><label>อายุ :</label></h3>
-         </div>
- 
-           <div class="col-xs-12 col-sm-1" style="padding-top: 12px;">
-             <input type="text" data-column="7" class="form-control column_filter" style="width: 80px;" id="col7_filter" name="start_age" value="0" />
-           </div>
-           <div class="col-xs-12 col-sm-1" style="padding-top: 12px;"><h3><label>ถึง</label></h3></div>
-           <div class="col-xs-12 col-sm-1" style="padding-top: 12px;">
-             <input type="text" data-column="8" class="form-control column_filter" style="width: 80px;" id="col8_filter" name="end_age" value="200" />
-           </div>
-           <div class="col-xs-12 col-sm-1" style="padding-top: 12px;">
-                  <h3><label>ปี</label></h3>
-           </div>
-
-       </div>
-
-
-       <div class="form-group row">
-         <div class="col-xs-12 col-sm-3">
-           <h3><label for="col5_filter">สถานะดำเนินการ:</label></h3>
-         </div>
-         <div class="col-xs-12 col-sm-3">
-           <select class="form-control" id="statusoper">
-              <option value="All">ทั้งหมด</option>
-              <option value="date_of_reg">วันที่ยื่นคำขอ</option>
-              <option value="date_of_pay">วันที่ได้รับการสงเคราะห์</option>
-             
-           </select>
-         </div>
-         <div class="col-xs-12 col-sm-4">
-
-           <div class="form-group" id="data_5" data-date-format="dd-mm-yyyy">
-             <div class="input-daterange input-group " id="datepicker">
-               <input type="text" class="input-sm form-control" name="start" value="" placeholder="เลือกตั้งแต่วันที่" style="height: 34px;" />
-               <span class="input-group-addon">ถึง</span>
-               <input type="text" class="input-sm form-control" name="end" value="" placeholder="เลือกถึงวันที่" style="height: 34px;"/>
-             </div>
-           </div>
-
-           <input type="hidden" data-column="3" class="column_filter" id="col3_filter">
-           <input type="hidden" data-column="4" class="column_filter" id="col4_filter">
-           <input type="hidden" data-column="5" class="column_filter" id="col5_filter">
-           <input type="hidden" data-column="6" class="column_filter" id="col6_filter">
-           <input type="hidden" data-column="10" class="column_filter" id="col10_filter">
-           <script type="text/javascript">
-
-            $('#data_5 .input-daterange').datepicker({
-             keyboardNavigation: false,
-             forceParse: false,
-             autoclose: true,
-             language: 'th',
-             thaiyear: true,
-             format: 'dd/mm/yyyy',
-             todayBtn: true
-           });
-
-         </script>
-       </div>
-       </div>
-
-        <div class="row">
-         <div class="col-xs-12 col-sm-3">
-          <h3><label for="col10_filter">หน่วยงาน (ผู้บันทึกข้อมูล):</label></h3>
-         </div>
-         <div class="col-xs-12 col-sm-6">
-             <?php
-           $user_id = get_session('user_id');
-           $app_id = 2;
-           $usrpm = $this->admin_model->chkOnce_usrmPermiss($app_id,$user_id);
-           ?>
-           <?php $usrm = $this->common_model->custom_query("SELECT * FROM usrm_org"); ?>
-           <select class="form-control" id="usrm_org">
-             <?php foreach($usrm as $key=>$value) {?>
-              <option value="<?php echo $value['org_id']; ?>" <?php if($usrpm['perm_view']!='All'){ if(get_session('org_id')!=$value['org_id']){ echo "disabled";} }; ?> ><?php echo $value['org_title']; ?></option>
-              <?php } ?>
-           </select>
-         </div>
-       </div>
-
-       <div class="row">
-          <div class="col-xs-12 col-sm-8">&nbsp;</div>
-          <div class="col-xs-12 col-sm-4 right" style="padding-right: 3px;">
-              <button id="filtersearch" class="btn btn-primary btn-save" type="button"  title="ค้นหา" style=" background-color: #2f4050;border: 1px;"><i class="fa fa-search" aria-hidden="true"></i> ค้นหา</button>
-              <a href="<?php echo base_url(''.uri_seg(1).'/'.uri_seg(2)); ?>"    class="btn btn-primary  btn-cancel" type="button" title="ค้นหา" style=" background-color: #2f4050;border: 1px;"><i class="fa fa-refresh" aria-hidden="true"></i> ล้างค่า</a>
+      <div class="form-group row ">
+        <div class="col-xs-12 col-sm-3">
+          <h3><label for="col1_filter">เลขประจำตัวประชาชน (ผู้สูงอายุ):</label></h3>
+        </div>
+        <div class="col-xs-12 col-sm-3">
+          <input data-column="1" type="text" class="form-control numberonly column_filter filter-pid" id="col1_filter" placeholder="เลขประจำตัวประชาชน (13 หลัก)" maxlength="13">
+        </div>
+        <div class="col-xs-12 col-sm-6">
+          <div class="form-check">
+            <h3>
+              <label class="form-check-label">
+                <input type="checkbox" class="form-check-input filter-disablepid" id="disablepid">
+                ไม่ระบุเลขบัตรประจำตัวประชาชน
+              </label>
+            </h3>
           </div>
-       </div>
+
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <div class="col-xs-12 col-sm-3">
+          <h3><label for="col2_filter">ชื่อตัว-ชื่อสกุล:</label></h3>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+          <input data-column="2" type="text" class="form-control column_filter filter-fullname" id="col2_filter" placeholder="ชื่อตัว-ชื่อสกุล">
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <div class="col-xs-12 col-sm-3" style="padding-top: 12px;">
+          <h3><label for="col3_filter">เพศ:</label></h3>
+        </div>
+        <div class="col-xs-12 col-sm-4" style="padding-top: 12px;">
+          <select class="form-control filter-gender" id="gender">
+            <option value="">ทั้งหมด</option>
+            <option value="1">ชาย (Male)</option>
+            <option value="2">หญิง (Female)</option>
+            <option value="0">ไม่ทราบ</option>
+            <option value="ไม่ระบุ">ไม่ระบุ</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <div class="col-xs-12 col-sm-3" style="padding-top: 12px;">
+          <h3><label for="col3_filter">สถานะดำเนินการ:</label></h3>
+        </div>
+        <div class="col-xs-12 col-sm-4" style="padding-top: 12px;">
+          <select class="form-control filter-status">
+            <option value="">ทั้งหมด</option>
+            <option value="1">รอการช่วยเหลือ</option>
+            <option value="2">ได้รับการสงเคราะห์แล้ว</option>
+            <option value="3" selected>รอการช่วยเหลือและได้รับการสงเคราะห์แล้ว</option>
+            <option value="4">ปฎิเสธ</option>
+          </select>
+        </div>
+      </div>
+
+
+      <div class="form-group row">
+        <div class="col-xs-12 col-sm-3">
+          <h3><label for="col5_filter">วันที่ยื่นคำขอ:</label></h3>
+        </div>
+        <div class="col-xs-12 col-sm-4">
+
+          <div class="form-group" id="data_5" data-date-format="dd-mm-yyyy">
+            <div class="input-daterange input-group" id="datepicker">
+              <input type="text" class="input-sm form-control  filter-req-start" name="start" value="" placeholder="เลือกตั้งแต่วันที่" style="height: 34px;" />
+              <span class="input-group-addon">ถึง</span>
+              <input type="text" class="input-sm form-control filter-req-end" name="end" value="" placeholder="เลือกถึงวันที่" style="height: 34px;" />
+            </div>
+          </div>
+          <script type="text/javascript">
+            $('#data_5 .input-daterange').datepicker({
+              keyboardNavigation: false,
+              forceParse: false,
+              autoclose: true,
+              language: 'th',
+              thaiyear: true,
+              format: 'dd/mm/yyyy',
+              todayBtn: true
+            });
+          </script>
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <div class="col-xs-12 col-sm-3">
+          <h3><label>วันที่ได้รับการสงเคราะห์<span class="span-required-filter-pay" style="font-weight: bold; color: red;"><span>:</label></h3>
+        </div>
+        <div class="col-xs-12 col-sm-4">
+          <?php
+            $currentDate = date('Y-m-d');
+            $date = new DateTime($currentDate);
+            $currentMonth = (int) $date->format('m');
+            $currentYear = (int) $date->format('Y');
+            if ($currentMonth >= 10) {
+                $startYear = $currentYear;
+                $endYear = $currentYear + 1;
+            } else {
+                $startYear = $currentYear - 1;
+                $endYear = $currentYear;
+            }
+            $startDateStr = "01/10/".($startYear+543);
+            $endDateStr = "30/09/".($endYear+543);
+          ?>
+          <div class="form-group" id="data_6" data-date-format="dd-mm-yyyy">
+            <div class="input-daterange input-group " id="datepicker2">
+              <input type="text" class="input-sm form-control filter-pay-start" name="start2" value="<?php echo $startDateStr;?>" placeholder="เลือกตั้งแต่วันที่" style="height: 34px;" />
+              <span class="input-group-addon">ถึง</span>
+              <input type="text" class="input-sm form-control filter-pay-end" name="end2" value="<?php echo $endDateStr;?>" placeholder="เลือกถึงวันที่" style="height: 34px;" />
+            </div>
+          </div>
+          <script type="text/javascript">
+            $('#data_6 .input-daterange').datepicker({
+              keyboardNavigation: false,
+              forceParse: false,
+              autoclose: true,
+              language: 'th',
+              thaiyear: true,
+              format: 'dd/mm/yyyy',
+              todayBtn: true
+            });
+          </script>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-3">
+          <h3><label for="col10_filter">หน่วยงาน (ผู้บันทึกข้อมูล):</label></h3>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+          <?php
+          $user_id = get_session('user_id');
+          $app_id = 2;
+          $session_org_id = get_session('org_id');
+          $usrpm = $this->admin_model->chkOnce_usrmPermiss($app_id, $user_id);
+          ?>
+          <?php $usrm = $this->common_model->custom_query("SELECT * FROM usrm_org"); ?>
+          <select class="form-control filter-usrm-org" id="usrm_org">
+            <?php
+              if($usrpm['perm_view'] == 'All'){ ?>
+                  <option value="" selected>ทั้งหมด</option>
+                  <?php foreach ($usrm as $key => $value) { ?>
+                    <option value="<?php echo $value['org_id']; ?>"><?php echo $value['org_title']; ?></option>
+                  <?php }?>      
+            <?php }else{ ?>
+                  <?php foreach ($usrm as $key => $value) { ?>
+                    <option value="<?php echo $value['org_id']; ?>" <?php if($session_org_id != $value['org_id']){echo "disabled"; }else{echo "selected";} ?> ><?php echo $value['org_title']; ?></option>
+                  <?php }?> 
+            <?php } ?>
+          </select>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-8">&nbsp;</div>
+        <div class="col-xs-12 col-sm-4 right" style="padding-right: 3px;">
+          <button id="filtersearch" class="btn btn-primary btn-save" type="button" title="ค้นหา" style=" background-color: #2f4050;border: 1px;"><i class="fa fa-search" aria-hidden="true"></i> ค้นหา</button>
+          <a href="<?php echo base_url('' . uri_seg(1) . '/' . uri_seg(2)); ?>" class="btn btn-primary  btn-cancel" type="button" title="ค้นหา" style=" background-color: #2f4050;border: 1px;"><i class="fa fa-refresh" aria-hidden="true"></i> ล้างค่า</a>
+        </div>
+      </div>
 
     </div><!-- End class="container-fluid"-->
   </div><!-- Endclass="well" -->
@@ -147,105 +179,32 @@
 <!-- End search -->
 
 <script>
+  $(function() {
 
-$(function () {
-
-    
     //เช็คกรณีไม่ระบุเลขบัตรประจำตัวประชาชน
-    $('#disablepid').on('change',function(){
-        if($(this).prop('checked')){
-          $('#col1_filter').prop('disabled','disabled');
-        }else{
-          $('#col1_filter').prop('disabled','');
-        }
-    });
-
-    //กดล้างค่า
-    $('#btnclear').click(function(){
-
-       $('.column_filter').each(function(){
-           $(this).val('');
-       });
-
-       $('#gender').each(function(){
-          if($(this).val()==0){
-            $(this).prop('selected','selected');
-          }
-       });
-
-       $('#statusoper').each(function(){
-          if($(this).val()=='All'){
-            $(this).prop('selected','selected');
-          }
-       });
-
-       $('input[name=start]').val('');
-       $('input[name=end]').val('');
-
-
-    });
-
-    //เลือกเพศ
-    $('#gender').change(function(){
-      // gender = $(this).val();
-      //      console.log('gen '+gender);
-          $('#col3_filter').val($(this).val());
-    });
-    
-    //เลือกถึงวันที่
-
-    $('#datepicker').change(function(){
-        var statusoper  = $('#statusoper').val();
-        var composedate = $('input[name=start]').val()+'_'+$('input[name=end]').val();
-         if(statusoper == 'date_of_reg'){
-           $('#col5_filter').val(composedate);
-         }
-       
-         else if(statusoper == 'date_of_pay'){
-           $('#col6_filter').val(composedate);
-         }
-    });
-
-   
-        //เลือกถึงวันที่
-    $('#datepicker').change(function(){
-        var statusoper  = $('#statusoper').val();
-        var composedate = $('input[name=start]').val()+'_'+$('input[name=end]').val();
-        $('#col5_filter').val('');
-        $('#col6_filter').val('');
-         if(statusoper == 'date_of_reg'){
-           $('#col5_filter').val(composedate);
-         }else if(statusoper == 'date_of_pay'){
-           $('#col6_filter').val(composedate);
-         }
-    });
-
-    //เลือกสถานะ
-    $('#statusoper').change(function(){
-      var statusoper  = $('#statusoper').val();
-      var composedate = !$('input[name=start]').val()?'*':$('input[name=start]').val()+'_'+$('input[name=end]').val();
-      $('#col5_filter').val('');
-      $('#col6_filter').val('');
-      if(statusoper == 'date_of_reg'){
-        $('#col5_filter').val(composedate);
-      }else if(statusoper == 'date_of_pay'){
-        $('#col6_filter').val(composedate);
-      }else{
-        $('input[name=start]').val('')
-        $('input[name=end]').val('')
+    $('.filter-disablepid').on('change', function() {
+      if ($(this).prop('checked')) {
+        $('#col1_filter').prop('disabled', 'disabled');
+      } else {
+        $('.filter-pid').prop('disabled', '');
       }
     });
 
-    //เลือก org
-   $('#usrm_org').change(function(){
-       $('#col10_filter').val($(this).val());
+    $('.filter-status').on('change', function() {
+      if(initRequiredFilterPay()){
+        $(".span-required-filter-pay").text("*");
+      }else{
+        $(".span-required-filter-pay").text("");
+      }
     });
+    $('.filter-status').change();
+  });
 
-    
- 
-
-
-});
-
+  function initRequiredFilterPay(){
+    let valRequiredFilterPay = $('.filter-status').val();
+    if(valRequiredFilterPay == "2" || valRequiredFilterPay == "3")
+      return true;
+    else
+      return false;
+  }
 </script>
-
